050e6727ee7a7bef28cf08dfdbc0359b7c17e786
Mohamed Magdy <Eng.M.Ezzeldeen@gmail.com>
2024-01-02 14:01:51 +0200

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,40 @@
 "use strict";
 
 /* ----------------------------------------------------------------------------- */
+/* Disable Inspect ------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------- */
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+
+}
+
+document.onkeydown = (e) => {
+
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+
+  if (
+
+    e.key === 'F12' ||
+
+    ctrlShiftKey(e, 'I') ||
+
+    ctrlShiftKey(e, 'J') ||
+
+    ctrlShiftKey(e, 'C') ||
+
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+
+    )
+
+  return false;
+
+};
+
+/* ----------------------------------------------------------------------------- */
 /* Loader ---------------------------------------------------------------------- */
 /* ----------------------------------------------------------------------------- */
 
